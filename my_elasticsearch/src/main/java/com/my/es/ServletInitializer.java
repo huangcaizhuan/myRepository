4acad5ca9d6b29ce941f617869fce965df2ab37f
huangcaizhuan <huangcaizhuan@DESKTOP-CKSKPGT>
2019-07-01 21:37:19 +0800

    初始化es项目

---
+++ my_elasticsearch/src/main/java/com/my/es/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.my.es;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyElasticsearchApplication.class);
+	}
+
+}
