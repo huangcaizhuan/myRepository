7115e5e66e0d4f2f61f8223e59ac2d4a461be379
huangcaizhuan <huangcaizhuan@DESKTOP-CKSKPGT>
2019-07-03 22:17:49 +0800

    增加客户端配置文件

---
+++ my_elasticsearch/src/main/java/com/my/es/MyConfig.java
@@ -0,0 +1,42 @@
+package com.my.es;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+	
+	
+	  @Bean 
+	  public TransportClient cleint() throws UnknownHostException { 
+		  Settings settings = Settings.builder().put("cluster.name", "hcz").build();
+	  
+	  		PreBuiltTransportClient client = new PreBuiltTransportClient(settings);
+	
+	  		int[] local_ports = {9300, 9301, 9302}; 
+	  		for (int port : local_ports) {
+	  			client.addTransportAddress(new TransportAddress(
+	  				InetAddress.getByName("localhost"), port )); 
+	  			}
+	  
+	  return client;
+	 }
+	
+//	@Bean
+//	public RestClient client(){
+//		RestClient restClient = RestClient.builder(
+//		        new HttpHost("localhost", 9300, "http"),
+//		        new HttpHost("localhost", 9301, "http"),
+//		        new HttpHost("localhost", 9302, "http")).build();
+//        return restClient;
+//	}
+}
--- my_elasticsearch/src/main/java/com/my/es/MyElasticsearchApplication.java
+++ my_elasticsearch/src/main/java/com/my/es/MyElasticsearchApplication.java
@@ -1,11 +1,38 @@
 package com.my.es;
 
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyElasticsearchApplication {
+	
+	@Autowired
+	private TransportClient client;
 
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	//查询接口
+	@GetMapping("/get/book")
+	public ResponseEntity get(@RequestParam(name="id",defaultValue = "")String id) {
+		GetResponse response = client.prepareGet("book", "novel", id).get();
+		if(!response.isExists()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity(response.getSource(),HttpStatus.OK);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyElasticsearchApplication.class, args);
 	}
